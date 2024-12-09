1a03a9900cf57076e2b3c27b435f3ede79533c88
Sappu95 <iamsapna.gautam.510@gmail.com>
2024-12-09 21:52:12 +0530

    first repo

---
+++ index.js
@@ -0,0 +1,9 @@
+let num = 3;
+function checkPrime(num){
+    for(let i=2; i<=num-1; i++){
+        if(num%i==0){
+            return false
+        }
+    }
+    return true
+}console.log(checkPrime(num))
